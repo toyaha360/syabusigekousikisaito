bfecdee6fe3fccde162c06a900331484cd4203da
toyaha360 <toyahashimizu@gmail.com>
2024-10-16 17:08:11 +0900

    Add files via upload

---
+++ mori.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // ここにJavaScriptコードを追加できます
+    const description = document.getElementById('description');
+    description.textContent = '〜世界で最もキマってるダチョウ〜';
+});
